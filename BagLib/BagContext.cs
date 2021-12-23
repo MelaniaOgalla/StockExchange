b92cd2bd69380cf688e002e2e49730cea652907d
Melania <melaniaogalla@gmail.com>
2021-12-23 13:32:11 +0100

    Initial app COMMIT

---
+++ BagLib/BagContext.cs
@@ -0,0 +1,42 @@
+﻿using BagLib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagLib
+{
+    public partial class BagContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Market> Markets { get; set; }
+
+        public DbSet<Stock> Stocks { get; set; }
+
+        public DbSet<Currency> Currencies { get; set; }
+
+        public DbSet<MyStock> MyStocks { get; set; }
+
+        public DbSet<BagUser> BagUsers { get; set; }
+
+
+        public BagContext(DbContextOptions<BagContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Bag;Trusted_Connection=True;");
+                //optionsBuilder.UseSqlServer("Server=localhost\\SQLDEVELOPER;Database=Blog;Trusted_Connection=True;");
+            }
+        }
+    }
+}
+
---
+++ BagLib/BagContext_Methods.cs
@@ -0,0 +1,70 @@
+﻿using BagLib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagLib
+{
+    public partial class BagContext : DbContext
+    {
+        public async Task <Currency> GetCurrencyAsync(string code)
+        {
+            return await Currencies.FirstOrDefaultAsync(d => d.Code == code);
+        }
+
+        public async Task<Country> GetCountryByNameAsync(string name)
+        {
+            return await Countries
+                .Include(d => d.Currencies)
+                .FirstOrDefaultAsync(d => d.Name == name || d.ApiName == name);
+        }
+
+        public async Task<Currency> InsertCurrencyAsync(Currency currency)
+        {
+            Add(currency);
+            await SaveChangesAsync();
+            return currency;
+        }
+
+        public async Task<Country> InsertCountryAsync(Country country)
+        {
+            Add(country);
+            await SaveChangesAsync();
+            return country;
+        }
+
+        public async Task<Country> GetCountryAsync(int? id)
+        {
+            return await Countries
+                .Include(d => d.Currencies)
+                .FirstOrDefaultAsync(m => m.CountryId == id);
+        }
+
+        public async Task<Stock> GetStockAsync(string symbol)
+        {
+            return await Stocks.FirstOrDefaultAsync(d => d.Ticket == symbol);
+        }
+
+        public async Task<Stock> InsertStockAsync(Stock stock)
+        {
+            Add(stock);
+            await SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<Market> GetMarketAsync(string name)
+        {
+            return await Markets.FirstOrDefaultAsync(d => d.Name == name);
+        }
+
+        public async Task<Market> InsertMarketAsync(Market market)
+        {
+            Add(market);
+            await SaveChangesAsync();
+            return market;
+        }
+    }
+}
