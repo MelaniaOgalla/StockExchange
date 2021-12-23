1dd8a6e10fa134890b0fd0dfc764bd970fd94557
Melania <melaniaogalla@gmail.com>
2021-12-23 14:02:17 +0100

    closes #3

---
+++ BagLib/Migrations/20211213145359_02_Currencies.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BagLib.Migrations
+{
+    public partial class _02_Currencies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrencyId",
+                table: "Markets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CountryCurrency",
+                columns: table => new
+                {
+                    CountriesCountryId = table.Column<int>(type: "int", nullable: false),
+                    CurrenciesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CountryCurrency", x => new { x.CountriesCountryId, x.CurrenciesId });
+                    table.ForeignKey(
+                        name: "FK_CountryCurrency_Countries_CountriesCountryId",
+                        column: x => x.CountriesCountryId,
+                        principalTable: "Countries",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CountryCurrency_Currencies_CurrenciesId",
+                        column: x => x.CurrenciesId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Markets_CountryId",
+                table: "Markets",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Markets_CurrencyId",
+                table: "Markets",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CountryCurrency_CurrenciesId",
+                table: "CountryCurrency",
+                column: "CurrenciesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Markets_Countries_CountryId",
+                table: "Markets",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "CountryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Markets_Currencies_CurrencyId",
+                table: "Markets",
+                column: "CurrencyId",
+                principalTable: "Currencies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Markets_Countries_CountryId",
+                table: "Markets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Markets_Currencies_CurrencyId",
+                table: "Markets");
+
+            migrationBuilder.DropTable(
+                name: "CountryCurrency");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Markets_CountryId",
+                table: "Markets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Markets_CurrencyId",
+                table: "Markets");
+
+            migrationBuilder.DropColumn(
+                name: "CurrencyId",
+                table: "Markets");
+        }
+    }
+}
---
+++ BagLib/Migrations/20211213151618_04_Currencies.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BagLib.Migrations
+{
+    public partial class _04_Currencies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CountryCurrency_Currencies_CurrenciesId",
+                table: "CountryCurrency");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Currencies",
+                newName: "CurrencyId");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrenciesId",
+                table: "CountryCurrency",
+                newName: "CurrenciesCurrencyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CountryCurrency_CurrenciesId",
+                table: "CountryCurrency",
+                newName: "IX_CountryCurrency_CurrenciesCurrencyId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Currencies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Symbol",
+                table: "Currencies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CountryCurrency_Currencies_CurrenciesCurrencyId",
+                table: "CountryCurrency",
+                column: "CurrenciesCurrencyId",
+                principalTable: "Currencies",
+                principalColumn: "CurrencyId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CountryCurrency_Currencies_CurrenciesCurrencyId",
+                table: "CountryCurrency");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Currencies");
+
+            migrationBuilder.DropColumn(
+                name: "Symbol",
+                table: "Currencies");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrencyId",
+                table: "Currencies",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrenciesCurrencyId",
+                table: "CountryCurrency",
+                newName: "CurrenciesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CountryCurrency_CurrenciesCurrencyId",
+                table: "CountryCurrency",
+                newName: "IX_CountryCurrency_CurrenciesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CountryCurrency_Currencies_CurrenciesId",
+                table: "CountryCurrency",
+                column: "CurrenciesId",
+                principalTable: "Currencies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
